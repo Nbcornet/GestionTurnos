93cf51b24ec5d67616a438dd96ce0ea287ff2f51
Nicolas Basualdo Cornet <75552717+Nbcornet@users.noreply.github.com>
2022-11-19 19:45:52 -0300

    Delete GestionDeTurnos directory

--- GestionDeTurnos/cuentas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-# from django.utils.translation import ugettext_lazy as _
-
-class CuentasConfig(AppConfig):
-    name = 'cuentas'
-
-    # def ready(self):
-    #     import cuentas.signals
